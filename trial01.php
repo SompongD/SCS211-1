1f04da9f52d6750e972835ecd0b17fda9cb7dcd3
thoyala <98438367+thoyala@users.noreply.github.com>
2023-06-14 20:47:40 +0700

    Prepare for Final
    Trial for Final Exam

---
+++ trial01.php
@@ -0,0 +1,12 @@
+<?php
+$txt1 = "Learn PHP";$txt2 = "W3Schools";$x=5;$y=4;
+echo "Nuttapong "."<h2 style='display:inline;'>".$txt1."</h2>"."  ".$txt2."<br>";
+echo $x +$y ."<br>";
+
+$colors = ["red","green","blue","yellow"];
+for($i=0;$i<count($colors);$i++){echo"$colors[$i]<br>";}
+foreach($colors as $value){echo "$value <br>";}
+
+
+
+?>
---
+++ trial02.php
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+    <?php include('layouts/header.php'); ?>
+    <div class="container mt-4">
+        <h1>Hello World</h1>
+    </div>
+    <?php include('layouts/footer.php'); ?>
+</body>
+
+</html>
